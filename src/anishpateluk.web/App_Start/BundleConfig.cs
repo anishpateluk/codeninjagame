4100f7ca3908875c970ee3d05961080f6f576541
Anish Patel <none@none>
2013-02-15 20:47:27 +0000

    bundled scripts
    --HG--
    branch : game
    rename : src/anishpateluk.web/js/vendor/easeljs-0.6.0.min.js => src/anishpateluk.web/js/vendor/easeljs-0.6.0.js
    rename : src/anishpateluk.web/js/vendor/jquery-1.9.1.min.js => src/anishpateluk.web/js/vendor/jquery-1.9.1.js

---
+++ src/anishpateluk.web/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace anishpateluk.web
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/scripts/game").Include("~/js/vendor/jquery-1.9.1.js",
+                                                                   "~/js/vendor/jquery.hotkeys.js",
+                                                                   "~/js/vendor/easeljs-0.6.0.js", "~/js/game.js"));
+        }
+    }
+}
