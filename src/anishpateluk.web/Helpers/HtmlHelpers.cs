41e2955461c4b03719bc745373c46672c3785dea
Anish Patel <none@none>
2013-02-16 18:36:36 +0000

    updated game view to use unbundled scripts when in debug
    --HG--
    branch : game

---
+++ src/anishpateluk.web/Helpers/HtmlHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace anishpateluk.web.Helpers
+{
+    using System.Web.Mvc;
+
+    public static class HtmlHelpers
+    {
+        public static bool IsDebug(this HtmlHelper htmlHelper)
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
