64d0d5a0aa3d8b6ff01a2a1b715864a4e14c407b
Anish Patel <none@none>
2013-02-16 21:33:01 +0000

    added glyph images and ToExpando extension method
    --HG--
    branch : game

---
+++ src/anishpateluk.web/Helpers/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace anishpateluk.web.Helpers
+{
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using System.Web.Mvc;
+
+    public static class Extensions
+    {
+        public static ExpandoObject ToExpando(this object anonymousObject)
+        {
+            IDictionary<string, object> anonymousDictionary = HtmlHelper.AnonymousObjectToHtmlAttributes(anonymousObject);
+            IDictionary<string, object> expando = new ExpandoObject();
+            foreach (var item in anonymousDictionary)
+                expando.Add(item);
+            return (ExpandoObject)expando;
+        }
+    }
+}
