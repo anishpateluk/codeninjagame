c2a84c22903a55cfc2b0268b3192635ee019417c
Anish Patel <anish.patel@thap.co.uk>
2013-03-30 17:42:29 +0000

    created separate game script to refactor game

---
+++ src/anishpateluk.web/js/game2.js
@@ -0,0 +1,11 @@
+﻿
+(function(window, $, undefined) {
+    //globals
+    var framesPerSecond = 30;
+    var canvas, world, stage, player, gameWidth;
+    var gameHeight = 300;
+    var requestedAssets = 0;
+    var totalAssets = 2;
+    
+
+})(window);
