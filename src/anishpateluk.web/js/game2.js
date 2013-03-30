e8e3f6c5e38eb0630286ab34ab93235a2a122f8b
Anish Patel <anish.patel@thap.co.uk>
2013-03-30 17:43:43 +0000

    added globals

--- src/anishpateluk.web/js/game2.js
+++ src/anishpateluk.web/js/game2.js
@@ -2,10 +2,20 @@
 (function(window, $, undefined) {
     //globals
     var framesPerSecond = 30;
-    var canvas, world, stage, player, gameWidth;
+    var game, canvas, world, stage, player, gameWidth;
     var gameHeight = 300;
     var requestedAssets = 0;
     var totalAssets = 2;
     
+
+    // sprite sheet image
+    var codeNinjaImg = new Image();
+    codeNinjaImg.height = 100;
+    codeNinjaImg.width = 5100;
+
+    // block image
+    var platformImg = new Image();
+    platformImg.height = 50;
+    platformImg.width = 300;
 
 })(window);
