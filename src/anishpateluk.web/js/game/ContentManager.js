04cdff8921496d4fc996783a2cee1b9c14c87ea4
Anish Patel <anish.patel@thap.co.uk>
2013-03-31 15:37:37 +0100

    factored out content management into separate script

---
+++ src/anishpateluk.web/js/game/ContentManager.js
@@ -0,0 +1,33 @@
+﻿
+
+function GameContentManager(callback) {
+    var self = this;
+
+    self.CodeNinjaImage = new Image();
+    self.PlatformImage = new Image();
+
+    var requestedAssets = 0;
+    var totalAssets = 2;
+
+    function assetLoaded() {
+        ++requestedAssets;
+        if (requestedAssets == totalAssets) {
+            callback();
+        }
+    }
+
+    function assetFailed(e) {
+        console.log(e);
+        throw new Error("Failed to Load asset");
+    }
+
+    self.loadAssets = function() {
+        self.CodeNinjaImage.onload = assetLoaded;
+        self.CodeNinjaImage.onerror = assetFailed;
+        self.CodeNinjaImage.src = "/GameAssets/images/cn_master.png";
+
+        self.PlatformImage.onload = assetLoaded;
+        self.PlatformImage.onerror = assetFailed;
+        self.PlatformImage.src = "/GameAssets/images/mario_platform.png";
+    };
+}
