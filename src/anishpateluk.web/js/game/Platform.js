0d3c99c7a13bc37cce7c610c7ca2a37a0ff67477
Anish Patel <anishpateldotnet@gmail.com>
2013-04-13 20:50:09 +0100

    added platform class

---
+++ src/anishpateluk.web/js/game/Platform.js
@@ -0,0 +1,26 @@
+﻿
+
+function Platform(platformImage) {
+	this.initialize(platformImage);
+}
+
+Platform.prototype = new createjs.Bitmap();
+Platform.prototype.Bitmap_initialize = Platform.prototype.initialize;
+Platform.prototype.initialize = function(platformImage) {
+	var self = this;
+
+	self.Bitmap_initialize(platformImage);
+	self.regY = 25;
+	self.regX = 150;
+
+	self.bounds = function() {
+		return {
+			height: platformImage.height,
+			width: platformImage.width,
+			regX: self.regX,
+			regY: self.regY,
+			x: self.x,
+			y: self.y
+		};
+	};
+}
