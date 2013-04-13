3d4c5bb93bc033c3685c7af637e72fa7ec1d32b7
Anish Patel <anishpateldotnet@gmail.com>
2013-04-13 21:55:00 +0100

    updated platform bounding box

--- src/anishpateluk.web/js/game/Platform.js
+++ src/anishpateluk.web/js/game/Platform.js
@@ -8,15 +8,14 @@ Platform.prototype = new createjs.Bitmap();
 Platform.prototype.Bitmap_initialize = Platform.prototype.initialize;
 Platform.prototype.initialize = function(platformImage) {
 	var self = this;
-
+	self.height = platformImage.height;
+	self.width = platformImage.width;
+	
 	self.Bitmap_initialize(platformImage);
-	self.regY = 25;
-	self.regX = 150;
-
 	self.bounds = function() {
 		return {
-			height: platformImage.height,
-			width: platformImage.width,
+			height: self.height,
+			width: self.width,
 			regX: self.regX,
 			regY: self.regY,
 			x: self.x,
