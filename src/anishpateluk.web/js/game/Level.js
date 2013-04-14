8aec4c0a289647110d3f97e398b80d56a5a60afb
Anish Patel <anishpateldotnet@gmail.com>
2013-04-14 16:06:46 +0100

    factored out level building into separate class

---
+++ src/anishpateluk.web/js/game/Level.js
@@ -0,0 +1,43 @@
+﻿
+
+function GameLevel(game, world, contentManager, width, height) {
+	var self = this;
+	
+	self.game = game;
+	self.world = world;
+	self.contentManager = contentManager;
+	self.width = width;
+	self.height = height;
+	self.platforms = [];
+	self.enemies = [];
+}
+
+GameLevel.prototype.addPlatform = function (x, y) {
+	var self = this;
+	var contentManager = self.contentManager;
+	var world = self.world;
+	var platforms = self.platforms;
+
+	x = Math.round(x);
+	y = Math.round(y);
+
+	var platform = new Platform(contentManager.PlatformImage);
+	platform.x = x;
+	platform.y = y;
+	platform.snapToPixel = true;
+
+	platforms.push(platform);
+	world.addChild(platform);
+};
+
+GameLevel.prototype.build = function() {
+	var self = this;
+	var level = self;
+	var game = self.game;
+
+	var width = game.width * 1.3;
+	for (var i = 0; i < width; i += 300) {
+		level.addPlatform(i, game.height);
+	}
+
+}
