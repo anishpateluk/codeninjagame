5a661bbc1cbc4dae27c40e9683f6d02d5fabb094
Anish Patel <anishpateldotnet@gmail.com>
2013-04-14 19:10:07 +0100

    created new test level

--- src/anishpateluk.web/js/game/Level.js
+++ src/anishpateluk.web/js/game/Level.js
@@ -1,13 +1,16 @@
 ﻿
 
-function GameLevel(game, world, contentManager, width, height) {
+function GameLevel(game, world, contentManager, schematic) {
 	var self = this;
 	
 	self.game = game;
 	self.world = world;
 	self.contentManager = contentManager;
-	self.width = width;
-	self.height = height;
+	self.schematic = schematic;
+	self.left = 0;
+	self.right = 0;
+	self.top = 0;
+	self.bottom = 0;
 	self.platforms = [];
 	self.enemies = [];
 }
@@ -34,10 +37,48 @@ GameLevel.prototype.build = function() {
 	var self = this;
 	var level = self;
 	var game = self.game;
+	
+
 
 	var width = game.width * 1.3;
 	for (var i = 0; i < width; i += 300) {
 		level.addPlatform(i, game.height);
+	}
+	
+	for (var i = 600; i < width; i += 300) {
+		level.addPlatform(i, game.height - 50);
+	}
+
+	for (var i = 600; i < width; i += 300) {
+		level.addPlatform(i, game.height - 100);
+	}
+
+	level.addPlatform(0, game.height - 150);
+	level.addPlatform(0, game.height - 200);
+	level.addPlatform(0, game.height - 250);
+	
+	for (var i = 900; i < width; i += 300) {
+		level.addPlatform(i, game.height - 150);
+	}
+	
+	for (var i = 1200; i < width; i += 300) {
+		level.addPlatform(i, game.height - 200);
+	}
+	
+	for (var i = 1500; i < width; i += 300) {
+		level.addPlatform(i, game.height - 250);
+	}
+	
+	for (var i = 1800; i < width; i += 300) {
+		level.addPlatform(i, game.height - 300);
+	}
+	
+	for (var i = 2100; i < width; i += 300) {
+		level.addPlatform(i, game.height - 350);
+	}
+	
+	for (var i = 2400; i < width; i += 300) {
+		level.addPlatform(i, game.height - 400);
 	}
 
 }
