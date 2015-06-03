7b240c723beb88415829df47db25e0b7d77c22f7
Anish Patel <anish.patel@ninjacode.co.uk>
2015-06-03 09:55:21 +0100

    moved game to index

--- src/anishpateluk.web/Controllers/HomeController.cs
+++ src/anishpateluk.web/Controllers/HomeController.cs
@@ -1,19 +1,12 @@
 ﻿namespace anishpateluk.web.Controllers
 {
+    using System.Web;
     using System.Web.Mvc;
 
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Game()
-        {
-            var userAgentString = HttpContext.Request.UserAgent != null
-                                      ? HttpContext.Request.UserAgent.ToLowerInvariant()
-                                      : string.Empty;
+        public ActionResult Index() {
+            var userAgentString = HttpContext.Request.UserAgent != null ? HttpContext.Request.UserAgent.ToLowerInvariant() : string.Empty;
 
             var isMobile = userAgentString.Contains("mobi");
             return View(isMobile);
